0c678eacde5d0dfadcd8acd706e32b1bbcd903d6
ghostnetic <github@smellyshiba.com>
2023-04-14 12:51:02 -0400

    Update background.js

--- background.js
+++ background.js
@@ -23,25 +23,33 @@ function parseHostsFile(content) {
 }
 
 function generateFilter(fileContents) {
+  let duplicatesRemoved = 0;
   const adblockRulesSet = new Set();
 
   fileContents.forEach(content => {
     const adblockRules = parseHostsFile(content);
-    adblockRules.forEach(rule => adblockRulesSet.add(rule));
+    adblockRules.forEach(rule => {
+      if (!adblockRulesSet.has(rule)) {
+        adblockRulesSet.add(rule);
+      } else {
+        duplicatesRemoved++;
+      }
+    });
   });
 
   const sortedRules = Array.from(adblockRulesSet).sort();
-  const header = generateHeader(sortedRules.length);
+  const header = generateHeader(sortedRules.length, duplicatesRemoved);
   const filterContent = [header, ...sortedRules].join('\n');
-  return filterContent;
+  return { filterContent, duplicatesRemoved };
 }
 
-function generateHeader(domainCount) {
+function generateHeader(domainCount, duplicatesRemoved) {
   const date = new Date().toISOString().slice(0, 10);
   return `# Title: AdBlock Filter Generator
 # Description: Chrome Extension to generate adblock syntax filter from multiple host files and blocklists
 # Created: ${date}
 # Domain Count: ${domainCount}
+# Duplicates Removed: ${duplicatesRemoved}
 #===============================================================`;
 }
 
@@ -49,7 +57,7 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   if (request.action === 'generateFilter') {
     try {
       console.log('Received file contents:', request.fileContents);
-      const filterContent = generateFilter(request.fileContents);
+      const { filterContent, duplicatesRemoved } = generateFilter(request.fileContents);
       console.log('Generated filter content:', filterContent);
 
       const blob = new Blob([filterContent], { type: 'text/plain' });
@@ -65,7 +73,7 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
 
       URL.revokeObjectURL(url);
 
-      sendResponse({ success: true });
+      sendResponse({ success: true, duplicatesRemoved });
     } catch (err) {
       console.error('Error generating filter:', err);
       sendResponse({ error: 'Error generating filter. Check the files and try again.' });
@@ -75,5 +83,3 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   // Required for async response handling.
   return true;
 });
-
-
