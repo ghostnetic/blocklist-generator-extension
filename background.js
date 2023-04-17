8aa4c4017c79391867af86b1b018e917d70500f2
ghostnetic <github@smellyshiba.com>
2023-04-17 13:35:05 -0400

    Update background.js

--- background.js
+++ background.js
@@ -50,7 +50,8 @@ function generateHeader(domainCount, duplicatesRemoved) {
 # Created: ${date}
 # Domain Count: ${domainCount}
 # Duplicates Removed: ${duplicatesRemoved}
-#===============================================================`;
+#=========================================================
+`;
 }
 
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
