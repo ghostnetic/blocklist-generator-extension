b288b51d12608cf4495694f8bc8410c3bc9627f8
ghostnetic <github@smellyshiba.com>
2023-04-17 21:40:31 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,81 +1,96 @@
 function parseHostsFile(content) {
-  const lines = content.split('\n');
-  const adblockRules = [];
-
-  lines.forEach(line => {
-    line = line.trim();
-
-    if (line.startsWith('#') || line.startsWith('!') || line === '') {
-      return;
-    }
-
+  const fileLines = content.split('\n');
+  const adblockRules = fileLines.filter(line => {
+    const trimmedLine = line.trim();
+    return !trimmedLine.startsWith('#') &&
+           !trimmedLine.startsWith('!') &&
+           trimmedLine !== '';
+  }).map(line => {
     if (line.startsWith('||') && line.endsWith('^')) {
-      adblockRules.push(line);
+      return line;
     } else {
-      const parts = line.split(/\s+/);
-      const domain = parts[parts.length - 1];
-      const rule = `||${domain}^`;
-      adblockRules.push(rule);
+      const lineParts = line.split(/\s+/);
+      const domain = lineParts[lineParts.length - 1];
+      return `||${domain}^`;
     }
   });
-
   return adblockRules;
 }
 
-function generateFilter(fileContents) {
+function removeRedundantRules(rules) {
+  const filteredRules = [];
+  const domainSet = new Set();
+
+  rules.forEach(rule => {
+    const domain = rule.slice(2, -1);
+    const baseDomain = domain.split('.').slice(-2).join('.');
+    if (!domainSet.has(baseDomain)) {
+      filteredRules.push(rule);
+      domainSet.add(baseDomain);
+    }
+  });
+
+  return filteredRules;
+}
+
+function generateFilter(fileContents, fileName = 'blocklist.txt') {
   let duplicatesRemoved = 0;
-  const adblockRulesSet = new Set();
+  const uniqueRules = new Set();
 
   fileContents.forEach(content => {
     const adblockRules = parseHostsFile(content);
     adblockRules.forEach(rule => {
-      if (!adblockRulesSet.has(rule)) {
-        adblockRulesSet.add(rule);
+      if (!uniqueRules.has(rule)) {
+        uniqueRules.add(rule);
       } else {
         duplicatesRemoved++;
       }
     });
   });
 
-  const sortedRules = Array.from(adblockRulesSet).sort();
-  const header = generateHeader(sortedRules.length, duplicatesRemoved);
+  const compressedRules = removeRedundantRules(Array.from(uniqueRules));
+  const domainsCompressed = uniqueRules.size - compressedRules.length;
+  duplicatesRemoved += domainsCompressed;
+  const sortedRules = compressedRules.sort();
+  const header = generateHeader(sortedRules.length, duplicatesRemoved, domainsCompressed);
   const filterContent = [header, ...sortedRules].join('\n');
-  return { filterContent, duplicatesRemoved };
+
+  const blob = new Blob([filterContent], { type: 'text/plain' });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = fileName;
+  a.style.display = 'none';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+
+  URL.revokeObjectURL(url);
+
+  return { filterContent, duplicatesRemoved, domainsCompressed };
 }
 
-function generateHeader(domainCount, duplicatesRemoved) {
+function generateHeader(domainCount, duplicatesRemoved, domainsCompressed) {
   const date = new Date().toISOString().slice(0, 10);
   return `# Title: AdBlock Filter Generator
 # Description: Chrome Extension to generate adblock syntax filter from multiple host files and blocklists
 # Created: ${date}
 # Domain Count: ${domainCount}
 # Duplicates Removed: ${duplicatesRemoved}
-#-------------------------------------------------------------------------
-`;
+# Domains Compressed: ${domainsCompressed}
+#===============================================================`;
 }
-
 
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   if (request.action === 'generateFilter') {
     try {
       console.log('Received file contents:', request.fileContents);
-      const { filterContent, duplicatesRemoved } = generateFilter(request.fileContents);
+      const { filterContent, duplicatesRemoved, domainsCompressed } = generateFilter(request.fileContents, request.fileName);
+
       console.log('Generated filter content:', filterContent);
 
-      const blob = new Blob([filterContent], { type: 'text/plain' });
-      const url = URL.createObjectURL(blob);
-
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = 'blocklist.txt';
-      a.style.display = 'none';
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-
-      URL.revokeObjectURL(url);
-
-      sendResponse({ success: true, duplicatesRemoved });
+      sendResponse({ success: true, duplicatesRemoved, domainsCompressed });
     } catch (err) {
       console.error('Error generating filter:', err);
       sendResponse({ error: 'Error generating filter. Check the files and try again.' });
